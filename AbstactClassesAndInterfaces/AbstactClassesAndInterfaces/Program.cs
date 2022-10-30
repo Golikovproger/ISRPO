0046964fb8b0bde0a0b8e4050dde546c06cbdd68
Golikovproger <maksimgolikov19@gmail.com>
2022-10-30 12:56:34 +0300

    4A TASK

---
+++ AbstactClassesAndInterfaces/AbstactClassesAndInterfaces/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    class Angle
+    {
+        int degrees;
+        float mins;
+        char way;
+
+        public Angle ()
+        {
+            degrees = 0;
+            mins = 0;
+            way = 'S';
+        }
+        public Angle (int degrees, float mins, char way)
+        {
+            this.degrees = degrees;
+            this.mins = mins;
+            this.way = way;
+        }
+
+       
+        public void SetValues (int degrees, float mins, char way)
+        {
+            this .degrees = degrees;
+            this.mins = mins;
+            this.way = way;
+        }
+
+        public void Print ()
+        {
+            Console.WriteLine($"{degrees} {mins} {way} ");
+
+        }
+
+        static void Main()
+        {
+            Angle a = new Angle (3, 5, 'N');
+            a.Print();
+            Angle b = new Angle();
+            b.SetValues(4, 8, 'S');
+            b.Print();
+        }
+    }
+}
